757253189f2697b83758809fe8c20a79c6468a59
IliaNiyazof <IFHRJCFY@yandex.ru>
2019-03-17 09:08:32 +0300

    delete

--- Django/adminapp/models/users.py
+++
@@ -1,8 +0,0 @@
-from authapp.models import ShopUser
-from authapp.forms import UpdateForm
-
-
-class ShopUserAdminEditForm(UpdateForm):
-    class Meta:
-        model = ShopUser
-        fields = '__all__'
--- Django/mainapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ProductCategory, Product, Person, Department
-
-admin.site.register(ProductCategory)
-admin.site.register(Product)
-admin.site.register(Person)
-admin.site.register(Department)
